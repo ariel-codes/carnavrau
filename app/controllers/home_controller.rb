eda3700314f760a2d91136318e437f27055a11f4
Ariel Santos <ariel@fluente.software>
2024-06-10 21:30:26 -0300

    Update & cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@
 class HomeController < ApplicationController
   def index
     blocks_by_day = Block.where("starts_at > ?", Time.zone.now.beginning_of_day - 12.hours)
-                    .order(:starts_at)
-                    .select("*, strftime('%d-%m-%Y', starts_at) AS starts_at_day")
-                    .group_by(&:starts_at_day)
-                    .transform_keys { Date.parse(_1) }
-    render Home::IndexView.new(blocks_by_day: )
+      .order(:starts_at)
+      .select("*, strftime('%d-%m-%Y', starts_at) AS starts_at_day")
+      .group_by(&:starts_at_day)
+      .transform_keys { Date.parse(_1) }
+    render Home::IndexView.new(blocks_by_day:)
   end
 end
--- app/jobs/seed_database_from_block_spreadsheet_job.rb
+++ app/jobs/seed_database_from_block_spreadsheet_job.rb
@@ -5,7 +5,7 @@ class SeedDatabaseFromBlockSpreadsheetJob < ApplicationJob
 
   SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1qfeq6F2SaaI0fMki2gPI33on1QnYgjrC8BBuD1W21Zg/gviz/tq?tqx=out:csv".freeze
 
-  def perform(download_spreadsheet= true)
+  def perform(download_spreadsheet = true)
     fetch_spreadsheet(download_spreadsheet)
       .to_csv
       .map { map_row(_1) }
@@ -26,20 +26,16 @@ class SeedDatabaseFromBlockSpreadsheetJob < ApplicationJob
     {day:, start_time:, name:, starting_point:, musical_styles:, target_audiences:, size:}
   end
 
-  def parse_csv(data)
-    CSV.parse(data, headers: true)
-  end
-
   def fetch_spreadsheet(download)
     return ImportedSpreadsheet.last if !download && ImportedSpreadsheet.empty?
 
     response = HTTPX.get(SPREADSHEET_URL)
 
-    case response.status
+    case (status = response.status)
     when 200
       ImportedSpreadsheet.create!(response_body: response.body.to_s)
     else
-      raise "Error fetching spreadsheet: #{response.status}"
+      raise "Error fetching spreadsheet: #{status}"
     end
   end
 end
